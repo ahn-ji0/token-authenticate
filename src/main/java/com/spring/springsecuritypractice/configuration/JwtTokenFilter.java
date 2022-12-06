54e4a0fba29781afb125d07a29f9a44939efd47b
Jiyoung <ahn.jiyoung15@gmail.com>
2022-12-06 15:25:47 +0900

    Fix: JwtTokenFilter

--- src/main/java/com/spring/springsecuritypractice/configuration/JwtTokenFilter.java
+++ src/main/java/com/spring/springsecuritypractice/configuration/JwtTokenFilter.java
@@ -31,13 +31,11 @@ public class JwtTokenFilter extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         // request의 header의 authorization 부분을 가져와 final String에 저장
         final String authorizationHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
-        log.info("1");
         //토큰이 없거나, Bearer 로 시작하지 않는 경우 다음 필터 호출
         if(authorizationHeader == null || !authorizationHeader.startsWith("Bearer ")){
             filterChain.doFilter(request,response);
             return;
         }
-        log.info("2");
 
         String token;
         try {
@@ -47,24 +45,15 @@ public class JwtTokenFilter extends OncePerRequestFilter {
             filterChain.doFilter(request,response);
             return;
         }
-        log.info("3");
 
         if(JwtTokenUtils.isExpired(token,secretKey)){
             filterChain.doFilter(request,response);
             return;
         }
-        log.info("4");
 
         UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(JwtTokenUtils.getUserName(token,secretKey), null, List.of(new SimpleGrantedAuthority("User")));
-        log.info("5");
-
         authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-        log.info("6");
-
         SecurityContextHolder.getContext().setAuthentication(authenticationToken); //권한 부여
-
-        log.info("7");
         filterChain.doFilter(request, response); //다음 체인으로 넘어간다.
-        log.info("8");
     }
 }

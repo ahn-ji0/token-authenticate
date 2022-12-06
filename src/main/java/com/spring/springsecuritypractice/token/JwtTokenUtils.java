d851168ebfeaaeeec984af8353d23ea2dc797ffd
Jiyoung <ahn.jiyoung15@gmail.com>
2022-12-06 13:25:46 +0900

    JwtTokenUtils

---
+++ src/main/java/com/spring/springsecuritypractice/token/JwtTokenUtils.java
@@ -0,0 +1,22 @@
+package com.spring.springsecuritypractice.token;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+import java.util.Date;
+
+public class JwtTokenUtils {
+
+    public static String createToken(String userName, String key, long expiredMs) {
+        Claims claims = Jwts.claims();
+        claims.put("userName",userName);
+
+        return Jwts.builder()
+                .setClaims(claims)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis()+expiredMs))
+                .signWith(SignatureAlgorithm.HS256, key)
+                .compact();
+    }
+}
